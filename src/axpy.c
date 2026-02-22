df91b378ec887bcf44ffb46a32e11ce476b2166c
lexxani_dev <lexxani@proton.me>
2026-02-22 14:51:55 +0100

    Added null-pointer handling

--- src/axpy.c
+++ src/axpy.c
@@ -1,8 +1,9 @@
 #include "vectoria.h"
+#include "stddef.h"
 
 void vct_axpy(int n, double alpha, const double *x, int incx, double *y, int incy)
 {
-    if (n <= 0 || incx == 0 || incy == 0)
+    if (n <= 0 || x == NULL || y == NULL || incx == 0 || incy == 0)
         return;
 
     for (int i = 0; i < n; i++, x += incx, y += incy)
@@ -13,8 +14,9 @@ void vct_axpy(int n, double alpha, const double *x, int incx, double *y, int inc
 
 void vct_axpy_copy(int n, double alpha, const double *x, int incx, const double *y, int incy, double *z, int incz)
 {
-    if (n <= 0 || incx == 0 || incy == 0 || incz == 0)
+    if (n <= 0 || x == NULL || y == NULL || incx == 0 || incy == 0 || incz == 0)
         return;
+
     for (int i = 0; i < n; i++, x += incx, y += incy, z += incz)
     {
         *z = alpha * (*x) + (*y);
