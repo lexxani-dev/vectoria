8613fd10d962c90c4974e155b3240036c4f61cce
lexxani_dev <lexxani@proton.me>
2026-02-22 15:21:27 +0100

    More guards, nicer loops

--- src/scal.c
+++ src/scal.c
@@ -1,21 +1,24 @@
 #include "vectoria.h"
+#include <stddef.h>
 
-void vct_scal(int n, double alpha, double *x)
+void vct_scal(int n, double alpha, double *x, int incx)
 {
-    if (n <= 0 || alpha == 1.0)
+    if (n <= 0 || x == NULL || incx == 0 || alpha == 1.0)
         return;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++, x += incx)
     {
-        x[i] = alpha * x[i];
+        *x = alpha * (*x);
     }
 }
 
-void vct_scal_copy(int n, double alpha, const double *x, double *y)
+void vct_scal_copy(int n, double alpha, const double *x, int incx, double *y, int incy)
 {
-    if (n <= 0)
+    if (n <= 0 || x == NULL || y == NULL || incx == 0 || incy == 0)
         return;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++, x += incx, y += incy)
     {
-        y[i] = alpha * x[i];
+        *y = alpha * (*x);
     }
 }
