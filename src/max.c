6dcf459cbc16243dfae63f6e9ed31e16eda0f3a5
lexxani_dev <lexxani@proton.me>
2026-02-22 15:05:08 +0100

    Refinment: Deleted logic for NaN / inf-handling. Correct max initialization. Made loop cleaner

--- src/max.c
+++ src/max.c
@@ -1,40 +1,25 @@
-#include <math.h>
-#include <stdlib.h>
 #include "vectoria.h"
+#include <math.h>
+#include <stddef.h>
 
-int vct_iamax(int n, const double *x)
+int vct_iamax(int n, const double *x, int incx)
 {
-    if (n <= 0 || x == NULL)
+    if (n <= 0 || x == NULL || incx == 0)
     {
         return -1;
     }
+
     int idx_max = 0;
-    double max_val;
-    if (!isnan(x[0]))
+    double max_val = fabs(*x);
+
+    for (int i = 1; i < n; i++, x += incx)
     {
-        max_val = fabs(x[0]);
-    }
-    else
-    {
-        max_val = 0.0;
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (isnan(x[i]))
+        if (fabs(*x) > max_val)
         {
-            continue;
-        }
-        else if (!isfinite(x[i]))
-        {
-            max_val = fabs(x[i]);
-            idx_max = i;
-            break;
-        }
-        else if (fabs(x[i]) > max_val)
-        {
-            max_val = fabs(x[i]);
+            max_val = fabs(*x);
             idx_max = i;
         }
     }
+
     return idx_max;
 }
