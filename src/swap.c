d081b77269bbe5303a2d8570a1fc935635584839
lexxani_dev <lexxani@proton.me>
2026-02-22 15:23:41 +0100

    Also more guards, nicer loops

--- src/swap.c
+++ src/swap.c
@@ -1,13 +1,15 @@
 #include "vectoria.h"
+#include <stddef.h>
 
-void vct_swap(int n, double *x, double *y)
+void vct_swap(int n, double *x, int incx, double *y, int incy)
 {
-    if (n <= 0)
+    if (n <= 0 || x == NULL || y == NULL || incx == 0 || incy == 0)
         return;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++, x += incx, y += incy)
     {
-        double tmp = y[i];
-        y[i] = x[i];
-        x[i] = tmp;
+        double tmp = *y;
+        *y = *x;
+        *x = tmp;
     }
-};
+}
