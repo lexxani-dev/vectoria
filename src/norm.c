392d275e33955b865afaace56f9d6078404b136e
lexxani_dev <lexxani@proton.me>
2026-02-22 15:16:27 +0100

    Minor fixes

--- src/norm.c
+++ src/norm.c
@@ -1,26 +1,17 @@
-#include <math.h>
 #include "vectoria.h"
+#include <stddef.h>
+#include <math.h>
 
-double vct_nrm2(int n, const double *x)
+double vct_nrm2(int n, const double *x, int incx)
 {
-    if (n <= 0)
+    if (n <= 0 || x == NULL || incx == 0)
         return 0.0;
+
     double nrm2 = 0.0;
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < n; i++, x += incx)
     {
-        nrm2 += x[i] * x[i];
+        nrm2 += (*x) * (*x);
     }
     return sqrt(nrm2);
-}
-
-double vct_absum(int n, const double *x)
-{
-    if (n <= 0)
-        return 0.0;
-    double sum = 0.0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += fabs(x[i]);
-    }
-    return sum;
 }
